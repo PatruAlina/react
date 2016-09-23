c5753c3f0ed3dab4429b4b94e7e529660bce85fb
PatruAlina <alina_rdp@yahoo.com>
2016-09-23 10:23:58 +0300

    react webapp test

---
+++ src/js/routes.js
@@ -0,0 +1,20 @@
+var React=require('react');
+var Router=require('react-router');
+var Route=Router.Route;
+var NotFoundRoute=Router.NotFoundRoute;
+var DefaultRoute=Router.DefaultRoute;
+var AppBaseView=require('./app/layout');
+var NotFoundView=require('./404/layout');
+var HomeView=require('./home/layout');
+var LoginView=require('./login/layout');
+
+
+var routes=(
+	<Route name="baseAppRouter" path="/" handler={AppBaseView}>
+		<NotFoundRoute handler={NotFoundView} />
+		<DefaultRoute handler={HomeView} />
+		<Route name="login" handler={LoginView} />
+	</Route>
+);
+
+module.exports=routes;
